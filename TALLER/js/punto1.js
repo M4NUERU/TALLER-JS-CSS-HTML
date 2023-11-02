c3f90511c27fedf972a714b6ff856b5590ed5dc4
rinconsebaz <126836757+rinconsebaz@users.noreply.github.com>
2023-11-02 18:31:29 -0500

    x

--- TALLER/js/punto1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Agrega un evento de escucha al campo de texto
-    document.getElementById("texto").addEventListener("input", function () {
-        var texto = this.value;
-        var contador = document.getElementById("contador");
-        contador.textContent = texto.length;
-    });
-});
-
